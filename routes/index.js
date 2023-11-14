5e249a0ed73404b8109fc3b4995bb2b2bd035ae4
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-11-14 22:07:52 +0000

    Create Route For /sign-up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',(req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/sign-up', (req, res, next) => {
+  res.render('signup', {
+    title: 'Sign up',
+  })
+})
 
 module.exports = router;
