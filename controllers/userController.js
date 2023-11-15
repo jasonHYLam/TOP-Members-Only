814f4e24aa25a9852954dcd631b509216b9c7b5b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-11-15 12:38:21 +0000

    Use CamelCase For Signup Form Input Values

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user");
 
+const { body, validationResult } = require('express-validator');
+
 exports.user_signup_get = asyncHandler( async (req, res, next) => {
   res.render('signup', {
     title: 'Sign up',
   })
 });
+
+exports.user_signup_post = [
+
+]
